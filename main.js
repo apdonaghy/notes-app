e0ec3d6be9a570a9b2b6975896d89124e5330b54
apdonaghy <adonaghy@seattleu.edu>
2020-03-05 21:24:19 -0800

    added close button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let notes = [];
+let container;
 
-const addNote = (event) => {
+function addNote(event) {
     event.preventDefault();
     let note = {
         title: document.getElementById('title').value,
@@ -10,24 +11,47 @@ const addNote = (event) => {
 
     document.querySelector('form').reset();
 
+    if (note['title'].length !== 0) {
+        // let notePad = `<h2>${note['title']}</h2><span class="delete"><i class="fas fa-times-circle"></i></span><p>${note['copy']}</p>`
+
+     
+
+       
+        let newNotePad = document.createElement('div');
+        newNotePad.setAttribute('id', 'user-note-pad')
+        // newNotePad.innerHTML = notePad;
 
 
+        let newH2 = document.createElement('h2')
+        newH2.innerHTML = note['title'];
+        newNotePad.appendChild(newH2);
 
-    let notePad = `<h2>${note['title']}</h2><p>${note['copy']}</p>`
+        let close = document.createElement('span')
+        close.setAttribute('id', 'close-this')
+        close.innerHTML = `<i class="fas fa-times-circle">`;
+        newNotePad.appendChild(close);
 
 
-    let newNotePad = document.createElement('div');
-    newNotePad.setAttribute('id', 'user-note-pad')
-    newNotePad.innerHTML = notePad;
+        let newP = document.createElement('p')
+        newP.innerHTML = note['copy'];
+        newNotePad.appendChild(newP);
 
+        container.appendChild(newNotePad);
 
-    let container = document.querySelector('.wrapper');
+        close.addEventListener("click", function (event) {
 
-    container.appendChild(newNotePad);
-    document.getElementById('title').focus();
+            document.getElementById("close-this")
+            this.parentElement.remove();
+    
+    
+          });
+
+        document.getElementById('title').focus();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    container = document.querySelector('.wrapper');
     document.getElementById('btn').addEventListener('click', addNote);
     document.getElementById('title').focus();
 });
